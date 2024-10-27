533c6f0d7274344f4dd04c095372acac8e485e20
Armas <elianacudriz@gmail.com>
2024-10-26 21:41:40 -0500

    se ajustan ultimos detalles y se agregan archivos js

--- JS/index.js
+++ JS/index.js
@@ -3,15 +3,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const titulo = document.querySelector('.titulo');
     titulo.addEventListener('click', () => {
         alert('Bienvenido a Visión Empresarial');
+        window.location.href = '../HTML/nosotros.html';
     });
 
-
-    const header = document.querySelector('header');
-    header.addEventListener('mouseenter', () => {
-        header.style.backgroundColor = '#027373';
-    });
-
-    header.addEventListener('mouseleave', () => {
-        header.style.backgroundColor = '#038C7F';
-    });
 });
--- JS/servicios.js
+++ JS/servicios.js
@@ -5,6 +5,7 @@ document.addEventListener('DOMContentLoaded', () => {
     serviceCards.forEach(card => {
         card.addEventListener('click', () => {
             alert(`Estás interesado en ${card.querySelector('h3').textContent}. Contáctanos para más información.`);
+            window.location.href = '../HTML/contactenos.html'
         });
     });
 });
